6fb4b9fe34c34b11004e380d25528505376097a7
Virtual <virtualll001@gmail.com>
2022-01-18 19:36:25 +0100

    Amenities

---
+++ HiddenVilla_Server/Model/Amenities.cs
@@ -0,0 +1,10 @@
+﻿namespace HiddenVilla_Server.Model
+{
+    public class Amenities
+    {
+        public int Id { get; set; }
+        public string AmenityName { get; set; }
+        public string Description { get; set; }
+
+    }
+}
