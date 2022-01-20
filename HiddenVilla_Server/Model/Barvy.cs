4d468908ab12c5635f89c587f75b6475e803c758
Virtual <virtualll001@gmail.com>
2022-01-20 23:13:46 +0100

    Procvičování - hřiště, myko

---
+++ HiddenVilla_Server/Model/Barvy.cs
@@ -0,0 +1,11 @@
+﻿namespace HiddenVilla_Server.Model
+{
+    public class Barvy
+    {
+        public int Id { get; set; }
+        public string BarvaJmeno { get; set; }
+        public string BarvaCSS { get; set; }
+        public string UrlRyby { get; set; }
+        public bool Aktivni {get; set;}
+    }
+}
---
+++ HiddenVilla_Server/Model/Houba.cs
@@ -0,0 +1,14 @@
+﻿namespace HiddenVilla_Server.Model
+{
+    public class Houba
+    {
+        public int Id { get; set; }
+        public string Jmeno { get; set; }
+        public string Stanoviste { get; set; }
+        public string Obdobi { get; set; }
+        public bool Jedla { get; set; }
+        
+    }    
+}
+
+//TODO - jedlost časem předělat na enum
