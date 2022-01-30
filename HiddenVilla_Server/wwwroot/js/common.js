bc5e8bf5580538a53547537cd546dd1c9231ad9f
Virtual <virtualll001@gmail.com>
2022-01-30 18:54:16 +0100

    ALERTY

---
+++ HiddenVilla_Server/wwwroot/js/common.js
@@ -0,0 +1,33 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operace proběhla úspěšně", {timeOut: 10000});
+    }
+    if (type === "error") {
+        toastr.error(message, "Operace selhala", {timeOut: 10000});
+    }
+}
+
+
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Operace se zdařila',
+            message,
+            'success' //Swal zobrazí success ikonu = tenhle řádek neměnit!
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Operace se nezdařila',
+            message,
+            'error'            
+        )
+    }
+}
+
+
+
+
+
+
